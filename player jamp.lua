1d24c2421012bb25fecf703d7d7c2a7605a940d0
souma022001-svg <souma022001@gmail.com>
2025-12-28 19:44:21 +0900

    Update player jamp.lua

--- player jamp.lua
+++ player jamp.lua
@@ -1,6 +1,6 @@
 return function(player, power)
     local character = player.Character or player.CharacterAdded:Wait()
     local humanoid = character:WaitForChild("Humanoid")
-    humanoid.JumpPower = power or 400 
+    humanoid.JumpPower = power or 590
     print(player.Name .. "のジャンプ力を" .. humanoid.JumpPower .. "に変更しました！")
 end
